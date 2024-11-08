b3b6769fc6a3f4b08185db539defa1bdb55b894a
bub.bl <bub.bl@outlook.com>
2024-11-08 18:15:57 +0100

    Fix replanish task error, trying to create the wrong type

--- Code/Cache/ObjectPool.cs
+++ Code/Cache/ObjectPool.cs
@@ -24,8 +24,8 @@ internal static class ObjectPool
 
 	public static T CloneObject<T>( T theObject, string classTypeName )
 	{
-		Log.Info("Clone instance for " + string.Join(", ", theObject.GetType(), classTypeName));
-		
+		Log.Info( "Clone instance for " + string.Join( ", ", theObject.GetType(), classTypeName ) );
+
 		var instance = GetInstance<T>( theObject.GetType() );
 		Cloning.CopyClassData( theObject, instance, classTypeName );
 		return instance;
@@ -41,39 +41,39 @@ internal static class ObjectPool
 	public static object GetInstance( Type classType )
 	{
 		var targetType = classType;
-		
+
 		classType = classType.GetCollectionType();
 		var classTypeName = classType.FullName!;
-		
-		if ( !_objectPool.ContainsKey(classTypeName) )
+
+		if ( !_objectPool.ContainsKey( classTypeName ) )
 		{
 			throw new RoverDatabaseException( $"there is no registered instance pool for the type {classTypeName} - " +
-				"are you using the wrong class type for this collection?" );
+			                                  "are you using the wrong class type for this collection?" );
 		}
 
 		if ( _objectPool[classTypeName].TypePool.TryTake( out var instance ) )
 			return instance;
 
-		Log.Info("Create instance for " + targetType.FullName);
-		
+		Log.Info( "Create instance for " + targetType.FullName );
+
 		// If we couldn't get an instance, then we just have to create a new one.
 		return GlobalGameNamespace.TypeLibrary.Create<object>( targetType );
 	}
 
-	public static T GetInstance<T>(Type classType)
+	public static T GetInstance<T>( Type classType )
 	{
 		// var baseClassType = classType;
 		var baseType = classType.GetCollectionType();
-		
-		Log.Info("GetInstance1: " + string.Join(", ", typeof(T), baseType, classType));
-		
+
+		Log.Info( "GetInstance1: " + string.Join( ", ", typeof(T), baseType, classType ) );
+
 		if ( _objectPool[baseType.FullName!].TypePool.TryTake( out var instance ) )
 			return (T)instance;
 
 		// If we couldn't get an instance, then we just have to create a new one.
 		// return new T();
 
-		Log.Info("GetInstance2: " + string.Join(", ", typeof(T), classType));
+		Log.Info( "GetInstance2: " + string.Join( ", ", typeof(T), classType ) );
 		return (T)GlobalGameNamespace.TypeLibrary.Create( classType.Name, classType );
 	}
 
@@ -84,15 +84,14 @@ internal static class ObjectPool
 	{
 		classType = classType.GetCollectionType();
 		var classTypeName = classType.FullName!;
-		
+
 		// Different collections might use the same type. So this is possible.
 		if ( _objectPool.ContainsKey( classTypeName ) )
 			return;
 
 		_objectPool[classTypeName] = new PoolTypeDefinition
 		{
-			ObjectType = classType,
-			TypePool = new ConcurrentBag<object>()
+			ObjectType = classType, TypePool = new ConcurrentBag<object>()
 		};
 	}
 
@@ -105,23 +104,26 @@ internal static class ObjectPool
 
 		_timeLastCheckedPool = now;
 
-		foreach (var poolPair in _objectPool)
+		foreach ( var poolPair in _objectPool )
 		{
-			if ( Config.CLASS_INSTANCE_POOL_SIZE - poolPair.Value.TypePool.Count >= Config.CLASS_INSTANCE_POOL_SIZE / 2)
+			if ( Config.CLASS_INSTANCE_POOL_SIZE - poolPair.Value.TypePool.Count >=
+			     Config.CLASS_INSTANCE_POOL_SIZE / 2 )
 			{
 				GameTask.RunInThreadAsync( () => ReplenishPoolType( poolPair.Key, poolPair.Value.ObjectType ) );
 			}
 		}
 	}
 
-	private static void ReplenishPoolType(string classTypeName, Type classType )
+	private static void ReplenishPoolType( string classTypeName, Type classType )
 	{
+		classType = classType.GetCollectionType();
+		
 		var concurrentList = _objectPool[classTypeName].TypePool;
 		var instancesToCreate = Config.CLASS_INSTANCE_POOL_SIZE - concurrentList.Count;
 
 		for ( var i = 0; i < instancesToCreate; i++ )
 		{
-			concurrentList.Add(GlobalGameNamespace.TypeLibrary.Create<object>( classType ));
+			concurrentList.Add( GlobalGameNamespace.TypeLibrary.Create<object>( classType ) );
 		}
 	}
 }
